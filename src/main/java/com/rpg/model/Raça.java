85abdd3ee73e129bd69536db905148346a0ecaab
João Orlando <joaoorlandojr7@gmail.com>
2022-11-08 16:12:41 -0300

    att model raça

--- src/main/java/com/rpg/model/Raça.java
+++ src/main/java/com/rpg/model/Raça.java
@@ -22,5 +22,39 @@ public class Raça {
 
     @Size(min = 3, max = 2000)
     private String biografia;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSubraça() {
+		return subraça;
+	}
+
+	public void setSubraça(String subraça) {
+		this.subraça = subraça;
+	}
+
+	public String getBiografia() {
+		return biografia;
+	}
+
+	public void setBiografia(String biografia) {
+		this.biografia = biografia;
+	}
+    
+    
 	
 }
